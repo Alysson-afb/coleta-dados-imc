17003ac8506a0bb060b24010b5625e03f8257259
Alysson Buchner <alysson.afb@gmail.com>
2026-05-29 18:22:17 -0300

    Desenvolvimento front-end

---
+++ pages/dados-idade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados - Idade</title>
+</head>
+<body>
+    <nav>
+        <a href="painel-adm.html"> <img src="logo.png" alt="Logo"> </a>
+        <menu>
+            <a href="visualizar-registros.php">Visualizar Registros</a>
+            <br>
+            <a href="dados.imc.php">Dados - IMC</a>
+            <br>
+            <a href="dados-idade.php">Dados - Idade</a>
+            <br>
+            <a href="dados-peso.php">Dados - Peso</a>
+        </menu>
+    </nav>
+    <footer>
+        Credito - Alysson e Renan - IFSul
+    </footer>
+</body>
+</html>
---
+++ pages/processar-insercao.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processar Inserção</title>
+</head>
+<body>
+    <nav>
+        <img src="" alt="">
+        <hr>
+    </nav>
+    <h2>Dados Coletados com sucesso. Obrigado pela participação.</h2>
+    <a href="painel-adm.html">Painel Administrativo</a>
+    <footer>
+            Credito - Alysson e Renan - IFSul
+    </footer>
+</body>
+</html>
